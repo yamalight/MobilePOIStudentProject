3fe9686cfef90bad3df440e2ef5bfc06dbf6900b
Tim Ermilov <yamalight@gmail.com>
2011-09-14 11:46:12 +0200

    new js libs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(function(){
+	$("#loadData").live('vclick', function(){
+		
 	
+		var query = "\
+			PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> \
+			PREFIX place: <http://linkedgeodata.org/ontology/> \
+			SELECT DISTINCT ?place WHERE { \
+			?place geo:geometry ?geo .\
+			?place a place:Restaurant . \
+			Filter(bif:st_intersects (?geo, bif:st_point ("+lng+", "+lat+"), 10)) \
+			} LIMIT 10 \
+		";
+	});
 })
